6d5686f025d1509f8c769c589d08e0b6d1a16ec4
fridde <f@hehl.se>
2017-06-03 18:45:09 +0200

    Outsourced Update class

--- src/DefaultUpdate.php
+++ src/DefaultUpdate.php
@@ -2,6 +2,7 @@
 /**
  * Contains the class Update.
  */
+
 namespace Fridde;
 
 use Fridde\Utility as U;
@@ -14,21 +15,21 @@ use Carbon\Carbon;
  */
 class DefaultUpdate
 {
-    /* @var $ORM \Fridde\Naturskolan  */
+    /* @var $ORM \Fridde\Naturskolan */
     protected $ORM;
     /** @var array The request data sent into the constructor */
     protected $RQ;
-    /** @var array  */
+    /** @var array */
     protected $Return = [];
-    /** @var array  */
+    /** @var array */
     protected $Errors = [];
 
     /* @var array */
-    protected const DEFAULT_METHOD_ARGUMENTS = [
-        "updateProperty"            => ["entity_class","entity_id","property","value"],
-        "batchUpdateProperties"     => ["array_of_updates"],
-        "createNewEntity"           => ["entity_class", "properties"],
-        "createNewEntityFromModel"  => ["entity_class", "property", "value", "model_entity_id"]
+    const DEFAULT_METHOD_ARGUMENTS = [
+        "updateProperty" => ["entity_class", "entity_id", "property", "value"],
+        "batchUpdateProperties" => ["array_of_updates"],
+        "createNewEntity" => ["entity_class", "properties"],
+        "createNewEntityFromModel" => ["entity_class", "property", "value", "model_entity_id"],
     ];
 
     /**
@@ -55,20 +56,25 @@ class DefaultUpdate
      */
     public function updateProperty(string $entity_class, $entity_id, string $property, $value)
     {
+        $value = $this->replaceIdWithObject($entity_class, $property, $value);
         $this->ORM->updateProperty($entity_class, $entity_id, $property, $value);
         $this->ORM->EM->flush();
+
+        return $this;
     }
 
     public function batchUpdateProperties(array $array_of_updates)
     {
         // array of entity_class, entity_id, property, value
-        foreach($array_of_updates as $update_args){
-            if(count(array_filter(array_keys($update_args), "is_string")) > 0){
+        foreach ($array_of_updates as $update_args) {
+            if (count(array_filter(array_keys($update_args), "is_string")) > 0) {
                 $update_args = U::pluck($update_args, self::getMethodArgs("updateProperty"));
             }
             call_user_func_array([$this->ORM, "updateProperty"], array_values($update_args));
         }
         $this->ORM->EM->flush();
+
+        return $this;
     }
 
     public function createNewEntityFromModel(string $entity_class, string $property, $value, $model_entity_id)
@@ -80,11 +86,12 @@ class DefaultUpdate
         $properties = [$property => $value];
 
         $properties_to_sync = $syncables[$entity_class] ?? [];
-        foreach($properties_to_sync as $property_name){
-            $method_name = "get" . $property_name;
+        foreach ($properties_to_sync as $property_name) {
+            $method_name = "get".$property_name;
             $properties[$property_name] = $model_entity->$method_name;
         }
-        $this->createNewEntity($entity_class, $properties);
+
+        return $this->createNewEntity($entity_class, $properties);
     }
 
     public function createNewEntity(string $entity_class, array $properties = [])
@@ -94,21 +101,26 @@ class DefaultUpdate
         $entity = $this->N->ORM->createNewEntity($entity_class, $properties);
         $this->N->ORM->EM->flush();
         $this->setReturn("new_id", $entity->getId());
+
+        return $this;
     }
 
-    protected function replaceIdsWithObjects($entity_class, $properties)
+    protected function replaceIdsWithObjects(string $entity_class, $properties)
     {
-        $object_required = $this->object_required ?? [];
-
-        foreach($properties as $property => &$value){
-            $replacements = $object_required[$entity_class] ?? [];
-            if(in_array($property, $replacements)){
-                if(!is_object($value)){
-                    $value = $this->findById($property, $value);
-                }
-            }
+        foreach ($properties as $property_name => &$value) {
+            $value = $this->replaceIdWithObject($entity_class, $property_name, $value);
         }
+
         return $properties;
+    }
+
+    protected function replaceIdWithObject(string $entity_class, string $property_name, $value)
+    {
+        $replacements = $this->object_required[$entity_class] ?? [];
+        if (in_array($property_name, $replacements) && !is_object($value)) {
+            return $this->findById($property_name, $value);
+        }
+        return $value;
     }
 
     /**
@@ -119,9 +131,9 @@ class DefaultUpdate
     public static function getMethodArgs(string $method_name, array $additional_args = [])
     {
         $method_args = self::DEFAULT_METHOD_ARGUMENTS + $additional_args;
+
         return $method_args[$method_name] ?? [];
     }
-
 
 
     /**
@@ -135,12 +147,14 @@ class DefaultUpdate
      */
     public function getReturn($key = null)
     {
-        if(empty($key)){
+        if (empty($key)) {
             $this->setReturn("onReturn");
             $this->setReturn("success", !$this->hasErrors());
             $this->setReturn("errors", $this->getErrors());
+
             return $this->Return;
         }
+
         return $this->Return[$key];
     }
 
@@ -148,29 +162,30 @@ class DefaultUpdate
      * Sets $Return[$key]  with either a given $value or with a value taken from the
      * initial request $RQ.
      *
-     * @param string|array  $key The key to set. If array each key-value pair
+     * @param string|array $key The key to set. If array each key-value pair
      *                           are the arguments for this function.
-     * @param mixed|null  $value  The value to set at $Return[$key].
+     * @param mixed|null $value The value to set at $Return[$key].
      */
     public function setReturn($key, $value = null)
     {
-        if(is_string($key)){
+        if (is_string($key)) {
             $key_value_pairs = [$key => $value];
         } else {
             $key_value_pairs = $key;
         }
-        foreach($key_value_pairs as $key => $value){
+        foreach ($key_value_pairs as $key => $value) {
             $this->Return[$key] = $value;
         }
+
         return $this;
     }
 
     public function setReturnFromRequest($keys)
     {
-        if(is_string($keys)){
+        if (is_string($keys)) {
             $keys = [$keys];
         }
-        foreach($keys as $key){
+        foreach ($keys as $key) {
             $this->setReturn($key, $this->RQ[$key]);
         }
     }
@@ -204,8 +219,6 @@ class DefaultUpdate
     {
         return !empty($this->getErrors());
     }
-
-
 
 
     /**
